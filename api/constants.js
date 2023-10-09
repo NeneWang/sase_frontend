c30ea87881e56a1e4146aa5a37493964b565656f
NelsonWang <wangnelson2@gmail.com>
2023-10-09 00:36:45 -0300

    constants

--- api/constants.js
+++ api/constants.js
@@ -1,4 +1,6 @@
 export const MEME_API = 'https://meme-api.com/gimme';
 
 
-export const BACKEND_API = 'http://127.0.0.1:8000/';
+// export const BACKEND_API = 'http://127.0.0.1:8000/';
+
+export const BACKEND_API = 'https://fsma7pi6gh.us-east-1.awsapprunner.com/';
