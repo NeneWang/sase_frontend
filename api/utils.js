1f664edfd987c75dbb9a254b3a2a5a5ff2535ef6
NelsonWang <wangnelson2@gmail.com>
2023-10-08 23:30:40 -0300

    utils push memes

--- api/utils.js
+++ api/utils.js
@@ -8,16 +8,12 @@ export async function giveRandomMeme() {
     // https://meme-api.com/gimme
     // console.log("attempted to fetch from ", MEME_API)
     try {
-        const response = await axios.get(MEME_API);
+        const response = await axios.get(BACKEND_API + `api/getMeme`);
         const memeData = response.data;
 
-        if (memeData.preview && memeData.preview.length > 0) {
-            const lastPreview = memeData.preview[memeData.preview.length - 1];
-            console.log("lastPreview", lastPreview)
+        const lastPreview = memeData.preview;
             return { lastPreview, url: memeData.url, title: memeData.title };
-        } else {
-            return { lastPreview: "#", url: 'No previews available.' };
-        }
+        
     } catch (error) {
         console.error('Error fetching meme:', error);
         return { lastPreview: "#", url: 'No previews available' };
